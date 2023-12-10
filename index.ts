808d6895e49a99624099482b9f92eb85cb420d68
Amir Sharif <budgeneration@gmail.com>
2023-12-10 09:56:00 -0800

    update logic

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { getSubtitles, getVideoDetails } from 'youtube-caption-extractor';
 
 console.log(Bun.argv);
 const url = Bun.argv.at(-1);
-const videoIDFromCLI = new URL(url).searchParams.get('v');
+const videoIDFromCLI = new URL(url!).searchParams.get('v');
 const lang = 'en'; // Optional, default is 'en' (English)
 
 if (!videoIDFromCLI) {
@@ -14,20 +14,29 @@ if (!videoIDFromCLI) {
 
 console.log(`Fetching ... ${videoIDFromCLI}`);
 
-// Fetching Subtitles
-const fetchSubtitles = async (videoID: string, lang = 'en') => {
-  try {
-    return;
-  } catch (error) {
-    console.error('Error fetching subtitles:', error);
-  }
-};
-
 const video = await getVideoDetails({ videoID: videoIDFromCLI, lang });
 const subtitleData = await getSubtitles({ videoID: videoIDFromCLI, lang });
-const transcript = subtitleData.reduce((acc, part) => {
-  return (acc += ' ' + part.text);
+
+const transcriptByMinute: { [key: number]: string } = {};
+subtitleData.forEach((part) => {
+  const timestampInSeconds = Math.floor(parseFloat(part.start));
+  const timestampPrefix = Math.floor(timestampInSeconds / 60);
+
+  if (transcriptByMinute[timestampPrefix]) {
+    transcriptByMinute[timestampPrefix] += ' ' + part.text;
+  } else {
+    transcriptByMinute[timestampPrefix] = part.text;
+  }
 }, '');
 
-const filename = `${filenamify(video.title)}.txt`;
-Bun.write(filename, transcript);
+function formatTimestamp(timestamp: number) {
+  const date = new Date(timestamp * 60000);
+  return date.toISOString().slice(13, 19);
+}
+
+let transcript = '';
+const filename = `./out/${filenamify(video.title)}.txt`;
+for (const [key, value] of Object.entries(transcriptByMinute)) {
+  transcript += `${formatTimestamp(parseInt(key, 10))}: ${value}\n`
+}
+Bun.write(filename, transcript.trim());
---
+++ prompt_2.md
@@ -0,0 +1,23 @@
+You will be provided with a transcript that is denoted by timestamp.
+As you read through I want you to generate 5-10 timestamps and then also mention the topics discussed in those timestamps.
+Prefix each of your headings with an emoji that reflects what is being discussed.
+
+The transcript will be provided with timestamps per minute of content.
+You will create a summary with the key points as well as reference the timestamp when those key points are discussed.
+
+```
+<<add transcript here>>
+```
+
+Here is an example format:
+
+00:00 🎯 The Navy SEALs use the "Micro Mission Rule" to sustain deep focus amid chaos and distractions.
+01:42 💡 Flow State depends on certainty, complete concentration, and clear goals.
+04:17 🧠 The Task Positive Network (TPN) in the brain helps maintain focus and adapt to shifting priorities.
+06:36 🚀 Creating "Certainty Windows" and executing "Micro Missions" within them boosts focus and motivation.
+09:05 📅 Distinguishing between Mission-critical and merely urgent tasks helps reduce uncertainty.
+10:58 ⏳ Committing to certainty windows and avoiding interruptions is essential for sustained focus and flow.
+12:38 🛣 Extending certainty windows over time increases productivity and the potential for achieving flow state.
+
+Do not include every single timestamp. Try to highlight only key parts of the video.
+Do not include more than 10 timestamps.
